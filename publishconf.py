9911427d37337f27a23b1d2cfcc1c26c4799a121
Flurando <2024110144@mails.szu.edu.cn>
2024-11-05 20:57:54 +0800

    modifying...

--- publishconf.py
+++ publishconf.py
@@ -1,18 +1,7 @@
-# pelicanconf.py
-
 AUTHOR = 'Flurando'
 SITENAME = 'My Blog(CN)'
-
-# Path to your content directory
 PATH = 'content'
-
-# Timezone
-TIMEZONE = 'America/New_York'
-
-# Default language
-DEFAULT_LANG = 'en'
-
-# Pagination
+DEFAULT_LANG = 'zh_CN'
 DEFAULT_PAGINATION = 10
 
 # Static paths
