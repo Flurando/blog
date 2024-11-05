e2c89271e12688fab02c9f087f0cf362ddfb0ceb
Flurando <2024110144@mails.szu.edu.cn>
2024-11-05 20:52:55 +0800

    first attempt on getting pelican github action work

---
+++ publishconf.py
@@ -0,0 +1,19 @@
+# pelicanconf.py
+
+AUTHOR = 'Flurando'
+SITENAME = 'My Blog(CN)'
+
+# Path to your content directory
+PATH = 'content'
+
+# Timezone
+TIMEZONE = 'America/New_York'
+
+# Default language
+DEFAULT_LANG = 'en'
+
+# Pagination
+DEFAULT_PAGINATION = 10
+
+# Static paths
+STATIC_PATHS = ['images', 'extra']
