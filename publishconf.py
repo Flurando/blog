54342f62646197d64c91a9f990d1a95c547b5255
Flurando <2024110144@mails.szu.edu.cn>
2025-02-14 23:34:43 +0800

    try rss again

--- publishconf.py
+++ publishconf.py
@@ -7,8 +7,7 @@ DEFAULT_PAGINATION = 10
 # Static paths
 STATIC_PATHS = ['images', 'extra']
 
-FEED_MAX_ITEMS = 15
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-FEED_USE_SUMMARY = False
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
